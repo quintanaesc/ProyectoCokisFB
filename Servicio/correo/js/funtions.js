b60663d966cfb5d4a342a03ce29470717cbe01b9
robertoquintana <2im3.quintana.esc.azul@gmail.com>
2023-05-17 20:01:14 -0600

    Se creo el archivo de funciones.js y se implemento la funcion de generar tabla

---
+++ Servicio/correo/js/funtions.js
@@ -0,0 +1,50 @@
+function genera_tabla() {
+    // Obtener la referencia del elemento body
+    var body = document.getElementsByTagName("section")[0];
+  
+    // Crea un elemento <table> y un elemento <tbody>
+    var tabla   = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+    //Crea encabezados
+
+   
+    var hilera = document.createElement("tr");
+    for (var j = 0; j < 6; j++) {
+        // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+        // texto sea el contenido de <td>, ubica el elemento <td> al final
+        // de la hilera de la tabla
+        var celda = document.createElement("td");
+        var textoCelda = document.createTextNode("Encabezado"+j);
+        celda.appendChild(textoCelda);
+        hilera.appendChild(celda);
+    }
+
+    // agrega la hilera al final de la tabla (al final del elemento tblbody)
+    tblBody.appendChild(hilera);
+
+    // Crea las celdas
+    for (var i = 0; i < 10; i++) {
+      // Crea las hileras de la tabla
+      var hilera = document.createElement("tr");
+      for (var j = 0; j < 6; j++) {
+        // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+        // texto sea el contenido de <td>, ubica el elemento <td> al final
+        // de la hilera de la tabla
+        var celda = document.createElement("td");
+        var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
+        celda.appendChild(textoCelda);
+        hilera.appendChild(celda);
+      }
+  
+      // agrega la hilera al final de la tabla (al final del elemento tblbody)
+      tblBody.appendChild(hilera);
+    }
+  
+    // posiciona el <tbody> debajo del elemento <table>
+    tabla.appendChild(tblBody);
+    // appends <table> into <body>
+    body.appendChild(tabla);
+    // modifica el atributo "border" de la tabla y lo fija a "2";
+    tabla.setAttribute("border", "2");
+  }
+  
