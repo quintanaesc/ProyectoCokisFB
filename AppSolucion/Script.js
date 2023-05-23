c558bd80e3c0ab37b99277f99811ba81ac841378
robertoquintana <2im3.quintana.esc.azul@gmail.com>
2023-05-23 15:15:49 -0600

    creacion de las funciones de scrip solucion

---
+++ AppSolucion/Script.js
@@ -0,0 +1,22 @@
+function aumentarCookieYRecargar() {
+    // Obtener el valor actual de la cookie 'id_empleado'
+    var valorCookie = parseInt(document.cookie.replace(/(?:(?:^|.;\s)id_empleado\s*\=\s*([^;]).$)|^.*$/, "$1")) || 0;
+  
+    // Aumentar el valor de la cookie en 1
+    valorCookie++;
+  
+    // Establecer la nueva cookie con el valor aumentado
+    document.cookie = 'id_empleado=' + valorCookie;
+  
+    // Recargar la página actual
+    location.reload();
+  }
+
+  function encuentraUsuario(){
+    //encuentra el valor del letrero donde se muestra el usuario entregando un string
+    var usuario = document.getElementById("resultadoPHP").getElementsByTagName("li")[0].textContent;
+    return usuario;
+  }
+  
+  // Iniciar el bucle que se ejecutará cada 10 segundos
+  setInterval(aumentarCookieYRecargar, 1000);
