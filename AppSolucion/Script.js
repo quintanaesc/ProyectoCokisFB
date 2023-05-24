3fe30894539b99620548181647de841a61690bd4
robertoquintana <2im3.quintana.esc.azul@gmail.com>
2023-05-23 18:38:34 -0600

    correccion de funcion aumentarCokkie

--- AppSolucion/Script.js
+++ AppSolucion/Script.js
@@ -1,22 +1,25 @@
-function aumentarCookieYRecargar() {
-    // Obtener el valor actual de la cookie 'id_empleado'
-    var valorCookie = parseInt(document.cookie.replace(/(?:(?:^|.;\s)id_empleado\s*\=\s*([^;]).$)|^.*$/, "$1")) || 0;
-  
-    // Aumentar el valor de la cookie en 1
-    valorCookie++;
-  
-    // Establecer la nueva cookie con el valor aumentado
-    document.cookie = 'id_empleado=' + valorCookie;
-  
-    // Recargar la página actual
-    location.reload();
-  }
+function encuentraUsuario(){
+  //encuentra el valor del letrero donde se muestra el usuario entregando un string
+  var usuario = document.getElementById("resultadoPHP").getElementsByTagName("li")[0].textContent;
+  return usuario;
+}
 
-  function encuentraUsuario(){
-    //encuentra el valor del letrero donde se muestra el usuario entregando un string
-    var usuario = document.getElementById("resultadoPHP").getElementsByTagName("li")[0].textContent;
-    return usuario;
-  }
+function aumentarCookieYRecargar() {
+  // Obtener el valor actual de la cookie 'id_empleado'
+  var valorCookie = parseInt(document.cookie.replace(/(?:(?:^|.*;\s*)id_empleado\s*\=\s*([^;]*).*$)|^.*$/, "$1")) || 0;
+
+  // Aumentar el valor de la cookie en 1
+  valorCookie++;
+
+  // Establecer la nueva cookie con el valor aumentado
+  document.cookie = 'id_empleado=' + valorCookie;
+
+  // Recargar la página actual
+  location.reload();
+}
+
+// Iniciar el bucle que se ejecutará cada 10 segundos
+setInterval(aumentarCookieYRecargar, 10000);
+  //cheerio
+
   
-  // Iniciar el bucle que se ejecutará cada 10 segundos
-  setInterval(aumentarCookieYRecargar, 1000);
