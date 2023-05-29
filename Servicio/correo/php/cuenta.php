2511d0ef58081e4f0e1fa26ca6add77e0205861f
robertoquintana <2im3.quintana.esc.azul@gmail.com>
2023-05-29 02:01:28 -0600

    funcion de cambio de datos de cuenta

--- Servicio/correo/php/cuenta.php
+++ Servicio/correo/php/cuenta.php
@@ -1,46 +1,44 @@
 <?php
-// Verificar si la cooki existe
-if (isset($_COOKIE['id_empleado'])) {
+// Establecer la conexión con la base de datos
+$host = 'localhost';
+$db = 'email_service';
+$user = 'root';
+$password = '';
 
-    // Obtiene el valor de la cookie
-    $idEmpleado = $_COOKIE['id_empleado'];
-
-    // Establecer la conexión con la base de datos
-    $host = 'localhost';
-    $db = 'email_service';
-    $user = 'root';
-    $password = '';
-    try {
-        $dsn = "mysql:host=$host;dbname=$db";
-        $pdo = new PDO($dsn, $user, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo '<script>alert("Conexión fallida");</script>';
-        die();
-    }
-
-    // Escapa el valor del ID empleado para prevenir inyección de SQL
-    $idEmpleado = $pdo->quote($idEmpleado);
-
-    // Realiza la consulta en la tabla "empleados"
-    $sql = "SELECT nickname FROM empleado WHERE id_empleado = $idEmpleado";
-    $result = $pdo->query($sql);
-
-    // Verifica si se encontró un resultado
-    if ($result->rowCount() > 0) {
-        // Obtiene el nombre de usuario del primer resultado
-        $row = $result->fetch();
-        $username = $row['nickname'];
-
-        // Muestra el nombre de usuario
-        echo '<li>' . $username . '</li>';
-    } else {
-        echo "No se encontró ningún empleado";
-    }
-
-    // Cierra la conexión a la base de datos
-    $pdo = null;
-} else {
-    echo "La cookie no existe.";
+try {
+  $conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+  echo "Error al conectar con la base de datos: " . $e->getMessage();
 }
+
+// Obtener el ID del empleado desde la cookie "id_empleado"
+$id_empleado = $_COOKIE['id_empleado'];
+
+// Obtener los datos del formulario
+$nombre_usuario = $_POST['username'];
+$cuenta_correo = $_POST['account'];
+$contrasena = $_POST['password'];
+$confirmar_contrasena = $_POST['pass_confirm'];
+
+if ($contrasena === $confirmar_contrasena) {
+  // Actualizar el registro del empleado en la tabla "empleado"
+  $query = "UPDATE empleado SET nickname = :nombre_usuario, cuenta = :cuenta_correo, pass = :contrasena WHERE id_empleado = :id_empleado";
+  $stmt = $conn->prepare($query);
+  $stmt->bindParam(':nombre_usuario', $nombre_usuario);
+  $stmt->bindParam(':cuenta_correo', $cuenta_correo);
+  $stmt->bindParam(':contrasena', $contrasena);
+  $stmt->bindParam(':id_empleado', $id_empleado);
+  $stmt->execute();
+
+  echo '<script>alert("Datos de usuario actualizados correctamente");</script>';
+  header('Location: ../correo.html');
+} else {
+  echo '<script>alert("Contraseña no coincide");</script>';
+  header('Location: ../correo.html');
+}
+
+// Cerrar la conexión
+$conn = null;
 ?>
+
